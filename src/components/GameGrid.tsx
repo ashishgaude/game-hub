bef2272ffbb4e52e5ee7f351e9ee0b36f44e50e9
ashishgaude <ashish.gaude4@gmail.com>
2024-06-11 00:43:05 +0530

    Implement infinite scroll

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,10 +1,11 @@
-import { Box, Button, SimpleGrid, Text } from "@chakra-ui/react";
+import { Box, Button, SimpleGrid, Spinner, Text } from "@chakra-ui/react";
 import { GameQuery } from "../App";
 import useGames from "../hooks/useGames";
 import GameCard from "./GameCard";
 import GameCardContainer from "./GameCardContainer";
 import GameCardSkeleton from "./GameCardSkeleton";
 import React from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 interface Props {
   gameQuery: GameQuery;
@@ -14,9 +15,13 @@ const GameGrid = ({ gameQuery }: Props) => {
   const { data, error, isLoading, isFetchingNextPage, fetchNextPage, hasNextPage } = useGames(gameQuery);
   const skeletons = [1, 2, 3, 4, 5, 6];
   if (error) <Text>{error.message}</Text>;
+
+  const fetchedGamesCount = data?.pages.reduce((total, page) => total + page.results.length, 0) || 0;
+
   return (
-    <Box padding={5}>
+    <InfiniteScroll dataLength={fetchedGamesCount} hasMore={!!hasNextPage} next={() => fetchNextPage()} loader={<Spinner />}>
       <SimpleGrid
+        padding={5}
         columns={{
           sm: 1,
           md: 2,
@@ -38,8 +43,7 @@ const GameGrid = ({ gameQuery }: Props) => {
           </GameCardContainer>)}
         </React.Fragment>)}
       </SimpleGrid >
-      <Button marginY={5} onClick={() => fetchNextPage()}>{isFetchingNextPage ? 'Loading...' : 'Load more'}</Button>
-    </ Box>
+    </InfiniteScroll>
   );
 };
 
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -26,7 +26,8 @@ const useGames = (gameQuery: GameQuery) =>
         page: pageParam
       }
     }),
-    getNextPageParam: (lastPage, allPages) => { return lastPage.next ? allPages.length + 1 : undefined }
+    getNextPageParam: (lastPage, allPages) => { return lastPage.next ? allPages.length + 1 : undefined },
+    staleTime: 24 * 60 * 60 * 100 // 24 hours
   }
   );
 
