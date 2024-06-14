a54cff346d476bc761058870e0c764c04308683c
ashishgaude <ashish.gaude4@gmail.com>
2024-06-14 12:17:54 +0530

    Fixed the nav bar

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,6 +1,6 @@
 import useGame from "../hooks/useGame";
 import { useParams } from "react-router-dom";
-import { Heading, SimpleGrid, Spinner, Text } from "@chakra-ui/react";
+import { GridItem, Heading, SimpleGrid, Spinner, Text } from "@chakra-ui/react";
 import ExpandableText from "../components/ExpandableText";
 import DefinationItem from "../components/DefinationItem";
 import CriticScore from "../components/CriticScore";
@@ -15,13 +15,17 @@ const GameDetailPage = () => {
   if (isLoading) return <Spinner />;
   if (error || !game) throw error;
   return (
-    <>
-      <Heading>{game.name}</Heading>
-      <ExpandableText>{game.description_raw}</ExpandableText>
-      <GameAttributes game={game} />
-      <GameTrailer gameId={game.id} />
-      <GameScreenshot gameId={game.id} />
-    </>
+    <SimpleGrid columns={{ base: 1, md: 2 }} spacing={5}>
+      <GridItem>
+        <Heading>{game.name}</Heading>
+        <ExpandableText>{game.description_raw}</ExpandableText>
+        <GameAttributes game={game} />
+      </GridItem>
+      <GridItem>
+        <GameTrailer gameId={game.id} />
+        <GameScreenshot gameId={game.id} />
+      </GridItem>
+    </SimpleGrid>
   );
 };
 
